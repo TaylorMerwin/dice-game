269453eb1425c02c9297ae87d3ea5e7843771a38
Taylor Merwin <taylor.merwin@gmail.com>
2023-07-13 13:48:49 -0700

    updated play button

---
+++ index.js
@@ -0,0 +1,5 @@
+function myClick() {
+    // makes id welcome page disappear and makes setup page appear
+    document.getElementById("welcomePage").style.display = "none";
+    document.getElementById("setupPage").style.display = "block";
+}
