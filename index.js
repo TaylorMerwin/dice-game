6bf13dd1a1316cea43da4c84c0e7a2643d5cf6e4
Taylor <taylor.merwin@gmail.com>
2023-07-16 13:22:54 -0700

    added game page

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+window.addEventListener("load", function () {
+  var element = document.getElementById("setupPage");
+  element.style.display = "none";
+});
+
+window.addEventListener("load", function () {
+    var element = document.getElementById("gamePage");
+    element.style.display = "none";
+  });
+
 function myClick() {
-    // makes id welcome page disappear and makes setup page appear
-    document.getElementById("welcomePage").style.display = "none";
-    document.getElementById("setupPage").style.display = "block";
+  // makes id welcome page disappear and makes setup page appear
+  document.getElementById("welcomePage").style.display = "none";
+  document.getElementById("gamePage").style.display = "block";
 }
